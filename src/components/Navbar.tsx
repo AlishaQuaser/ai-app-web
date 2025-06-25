eb68120182734776c843b9c3f09c7273171c3c8c
Alisha Quaser <alisha.infomover@gmail.com>
2025-06-26 04:12:02 +0530

    add files

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
 
   return (
     <nav className="bg-slate-800 shadow-lg sticky top-0 z-50">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-10">
         <div className="flex justify-between h-16">
           <div className="flex">
             <div className="flex-shrink-0 flex items-center">
